24dec3494941ac3e8a7d320a4cc8d424991a5324
Choi jaemin <panzzang518@naver.com>
2022-08-22 17:07:50 +0900

    Header 로고 및 폰트 적용

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <template lang="">
   <header>
+    <Logo />
     <div class="nav nav-pills">
       <div v-for="nav in navigations" :key="nav.name" class="nav-item">
         <RouterLink :to="nav.href" active-class="active" class="nav-link">
@@ -10,7 +11,12 @@
   </header>
 </template>
 <script>
+import Logo from "~/components/Logo";
+
 export default {
+  components: {
+    Logo,
+  },
   data() {
     return {
       navigations: [
@@ -31,4 +37,14 @@ export default {
   },
 };
 </script>
-<style lang=""></style>
+<style lang="scss" scoped>
+header {
+  height: 70px;
+  padding: 0 40px;
+  display: flex;
+  align-items: center;
+  .logo {
+    margin-right: 40px;
+  }
+}
+</style>
