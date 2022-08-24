fc241bb871854466f6815f543c614b2cba792f36
Choi jaemin <panzzang518@naver.com>
2022-08-24 15:42:25 +0900

    Footer, 영화 상세 페이지 정보 가져오기

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template lang="">
   <Header> </Header>
   <RouterView></RouterView>
+  <Footer></Footer>
 </template>
 
 <script>
 import Header from "~/components/Header";
+import Footer from "~/components/Footer";
+
 export default {
   components: {
     Header,
+    Footer,
   },
 };
 </script>
--- src/store/movie.js
+++ src/store/movie.js
@@ -1,12 +1,13 @@
 import axios from "axios";
-import _uniqBy from 'lodash/uniqBy'
+import _uniqBy from "lodash/uniqBy";
 
 export default {
   namespaced: "true",
   state: () => ({
     movies: [],
-    message: "",
+    message: "Search for the movie title!",
     loading: false,
+    theMovie: {}
   }),
   getters: {},
   mutations: {
@@ -22,6 +23,11 @@ export default {
   },
   actions: {
     async searchMovies({ state, commit }, payload) {
+      if (state.loading) return;
+      commit("updateState", {
+        message: "",
+        loading: true,
+      });
       try {
         const res = await _fetchMovie({
           ...payload,
@@ -29,8 +35,7 @@ export default {
         });
         const { Search, totalResults } = res.data;
         commit("updateState", {
-          movies: _uniqBy(Search, 'imdbID'),
-
+          movies: _uniqBy(Search, "imdbID"),
         });
 
         const total = parseInt(totalResults, 10);
@@ -39,42 +44,71 @@ export default {
         //추가 요청 전송
         if (pageLength > 1) {
           for (let page = 2; page <= pageLength; page += 1) {
-            if (page > payload.number / 10) break
+            if (page > payload.number / 10) break;
             const res = await _fetchMovie({
               ...payload,
-              page:page
+              page: page,
             });
             const { Search } = res.data;
             commit("updateState", {
-              movies: [...state.movies, ..._uniqBy(Search, 'imdbID')],
+              movies: [...state.movies, ..._uniqBy(Search, "imdbID")],
             });
           }
         }
-      } catch (message){
-        commit('updateState',{
+      } catch (message) {
+        commit("updateState", {
           movies: [],
-          message
+          message,
+        });
+      } finally {
+        commit("updateState", {
+          loading: false,
+        });
+      }
+    },
+    async searchMovieWithId({ state, commit }, payload) {
+      if (state.loading) return;
+      commit("updateState", {
+        theMovie: {},
+        loading: true,
       });
+
+      try {
+        const res = await _fetchMovie(payload);
+        commit('updateState',{
+          theMovie: res.data(),
+        })
+      } catch (error) {
+        commit('updateState',{
+          theMovie: {},
+        })
+      }finally {
+        commit('updateState',{
+          loading: false,
+        })
       }
     },
   },
 };
 
 function _fetchMovie(payload) {
-  const {title, type, year, page} = payload;
+  const { title, type, year, page, id } = payload;
   const OMDB_API_KEY = "7035c60c";
-  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
+  const url = id
+    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
+    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
 
   return new Promise((resolve, reject) => {
-    axios.get(url)
+    axios
+      .get(url)
       .then((res) => {
         if (res.data.Error) {
           reject(res.data.Error);
         }
-        resolve (res);
+        resolve(res);
       })
-      .catch((err)=>{
+      .catch((err) => {
         reject(err.message);
-      })
+      });
   });
 }
